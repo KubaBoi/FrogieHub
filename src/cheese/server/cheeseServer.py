519b8c559ec6a3b24f2a51604e0e15b9b56e946b
KubaBoi <jakubanderle@outlook.cz>
2022-03-11 07:07:21 +0100

    debug

--- src/cheese/server/cheeseServer.py
+++ src/cheese/server/cheeseServer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.log import Log
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
 import threading
