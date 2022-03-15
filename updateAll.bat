a01ccc26391d2e8c70124a595e202e6178b3c038
KubaBoi <jakubanderle@outlook.cz>
2022-03-15 04:26:29 +0100

    Remove log

--- updateAll.bat
+++ updateAll.bat
@@ -4,19 +4,21 @@ python cheeser.py -b FrogieCheese
 python cheeser.py -b CheeseDocumentation
 python cheeser.py -b FrogieCloudos
 
+set message=Remove log
+
 cd projects/FrogieHub
 git add *
-git commit -m "Automatic commit"
+git commit -m "%message%"
 git push
 cd ../FrogieCheese
 git add *
-git commit -m "Automatic commit"
+git commit -m "%message%"
 git push
 cd ../CheeseDocumentation
 git add *
-git commit -m "Automatic commit"
+git commit -m "%message%"
 git push
 cd ../FrogieCloudos
 git add *
-git commit -m "Automatic commit"
+git commit -m "%message%"
 git push
