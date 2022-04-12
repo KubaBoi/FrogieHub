eb40ca1106d6c1123af5820c1da62db2aae1c1f2
KubaBoi <jakubanderle@outlook.cz>
2022-04-12 19:10:31 +0200

    Remove log

--- src/cheese/modules/cheeseRepository.py
+++ src/cheese/modules/cheeseRepository.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 import inspect
 
 from cheese.resourceManager import ResMan
@@ -35,10 +36,18 @@ class CheeseRepository:
 
         if (userRepository == "passwordRepository"):
             return PasswordRepositoryImpl.findNewId(args)
+        elif (userRepository == "passwordRepository"):
+            return PasswordRepositoryImpl.findNewId(args)
+        elif (userRepository == "serviceRepository"):
+            return ServiceRepositoryImpl.findNewId(args)
         elif (userRepository == "serviceRepository"):
             return ServiceRepositoryImpl.findNewId(args)
         elif (userRepository == "tokenRepository"):
             return TokenRepositoryImpl.findNewId(args)
+        elif (userRepository == "tokenRepository"):
+            return TokenRepositoryImpl.findNewId(args)
+        elif (userRepository == "userRepository"):
+            return UserRepositoryImpl.findNewId(args)
         elif (userRepository == "userRepository"):
             return UserRepositoryImpl.findNewId(args)
     @staticmethod
@@ -128,18 +137,6 @@ class CheeseRepository:
 
 
     @staticmethod
-    def findNewId(args):
-        userRepository = CheeseRepository.findUserRepository()
-
-        if (userRepository == "passwordRepository"):
-            return PasswordRepositoryImpl.findNewId(args)
-        elif (userRepository == "serviceRepository"):
-            return ServiceRepositoryImpl.findNewId(args)
-        elif (userRepository == "tokenRepository"):
-            return TokenRepositoryImpl.findNewId(args)
-        elif (userRepository == "userRepository"):
-            return UserRepositoryImpl.findNewId(args)
-    @staticmethod
     def save(args):
         userRepository = CheeseRepository.findUserRepository()
 
@@ -213,6 +210,8 @@ class CheeseRepository:
                     newArgs.append(str(arg))
             elif (type(arg) is list):
                 newArgs.append("(" + ",".join(CheeseRepository.getTypeOf(arg)) + ")")
+            elif (type(arg) is datetime):
+                newArgs.append("'" + datetime.strftime(arg, "%d-%m-%Y %H:%M:%S") + "'")
             else:
                 newArgs.append(str(arg))
         return newArgs
