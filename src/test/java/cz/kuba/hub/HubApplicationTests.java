e94f3e829a16daf0965e2ed435bb374c156b6d4f
Jakub Anderle <anderle@ica.cz>
2025-04-11 13:43:25 +0200

    init spring

---
+++ src/test/java/cz/kuba/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kuba.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
